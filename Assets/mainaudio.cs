6af0c3028e6e0dfd5c69f5fa300ddbe732255870
VishnuD382 <vishnudhanasekaran9@gmail.com>
2020-10-23 12:50:24 -0400

    sounds_fixed

---
+++ Assets/mainaudio.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class mainaudio : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioClip MainOfficeNoise;
+
+    public AudioSource audioOffice;
+
+    public AudioMixerSnapshot idleSnapshot;
+    public AudioMixerSnapshot auxInSnapshot;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("MainOffice"))
+        {
+            
+            idleSnapshot.TransitionTo(0.5f);
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("MainOffice"))
+        {
+            auxInSnapshot.TransitionTo(0.5f);
+
+        }
+    }
+
+
+
+}
