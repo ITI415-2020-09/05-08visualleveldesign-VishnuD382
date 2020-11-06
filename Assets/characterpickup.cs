2436a767e2b0a62a0ebcdc05efa2ce9e414ce852
VishnuD382 <vishnudhanasekaran9@gmail.com>
2020-11-06 15:43:04 -0500

    working with sounds 2

---
+++ Assets/characterpickup.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class characterpickup : MonoBehaviour
+{
+    public AudioClip collectSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // text goes in here.
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            AudioSource.PlayClipAtPoint(collectSound, transform.position);
+            other.gameObject.SetActive(false);
+        }
+
+    }
+
+}
