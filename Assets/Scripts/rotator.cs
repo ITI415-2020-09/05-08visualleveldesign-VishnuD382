db31a0b6dca7f656f893297d2fa2b5ea5d1fa1f7
VishnuD382 <vishnudhanasekaran9@gmail.com>
2020-11-06 17:17:22 -0500

    small edits

--- Assets/Scripts/rotator.cs
+++ Assets/Scripts/rotator.cs
@@ -9,6 +9,7 @@ public class rotator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+
+        transform.Rotate(new Vector3(15, 30, 45) * UnityEngine.Time.deltaTime);
     }
 }
