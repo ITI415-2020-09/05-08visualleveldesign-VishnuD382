32f42940d241d3c9bf3f0bcafd7f84866dfc37b6
VishnuD382 <vishnudhanasekaran9@gmail.com>
2020-11-06 16:13:28 -0500

    added scripts

--- Assets/Scripts/characterpickup.cs
+++ Assets/Scripts/characterpickup.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class characterpickup : MonoBehaviour
 {
     public AudioClip collectSound;
+    public Text scoreGT;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject scoreGO = GameObject.Find("Score");
+        scoreGT = scoreGO.GetComponent<Text>();
+        scoreGT.text = "0";
     }
 
     // Update is called once per frame
@@ -26,6 +30,12 @@ public class characterpickup : MonoBehaviour
         {
             AudioSource.PlayClipAtPoint(collectSound, transform.position);
             other.gameObject.SetActive(false);
+
+            int score = int.Parse(scoreGT.text);
+            score += 1;
+            scoreGT.text = score.ToString();
+
+            Remaining.score = Remaining.score - 1;
         }
 
     }
